1eca2754b4bef0dbf4302b3701d2354b18e29bc8
nakano57 <hiroto856@gmail.com>
2021-10-14 21:07:22 +0900

    add update time

--- get_menu.py
+++ get_menu.py
@@ -2,9 +2,13 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+import datetime
+
+dt_now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
 
 with open('menu.csv', 'w') as f:
     writer = csv.writer(f)
+    writer.writerow(["#Last Update"+str(dt_now)])
     writer.writerow(['#menu_name', 'category', 'price', 'kcal'])
 
     base = "https://www.sukiya.jp/"
@@ -16,7 +20,7 @@ with open('menu.csv', 'w') as f:
     for product in soup.find_all("div", class_="product_menu"):
         for i in product.find_all("li", class_="menu_item"):
             try:
-                print(i.find("a")['href'])
+                # print(i.find("a")['href'])
                 urls.append(base + i.find("a")['href'])
             except TypeError as e:
                 print(e)
@@ -53,7 +57,7 @@ with open('menu.csv', 'w') as f:
         sec = soup.find('div', id='sec_dish_nums')
 
         category = urlparse(u).path.split('/')[4]
-        print(category)
+        # print(category)
 
         for n in sec.find_all('li', class_='clr'):
             menu_name = base_menu_name + n.find('dt').get_text(strip=True)
