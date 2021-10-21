772a664a8f3cb34b1670fe0834cfabb306b22e53
Hiroto Nakano <40960166+nakano57@users.noreply.github.com>
2021-10-21 06:44:56 +0000

    update

--- get_menu.py
+++ get_menu.py
@@ -6,48 +6,47 @@ import datetime
 
 dt_now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
 
-with open('menu.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(["#Last Update"+str(dt_now)])
-    writer.writerow(['#menu_name', 'category', 'price', 'kcal'])
+base = "https://www.sukiya.jp/"
 
-    base = "https://www.sukiya.jp/"
-    urls = []
+url = "https://www.sukiya.jp/menu/in/gyudon/"
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'html.parser')
+lnav = soup.find(id='lnav_menu_in')
 
-    res = requests.get('https://www.sukiya.jp/menu/in/gyudon/')
+menu_urls = []
+for item in lnav.find_all('dd', class_='hd'):
+    menu_urls.append(base+item.find('a').get('href'))
+
+
+urls = []
+for url in menu_urls:
+    res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
-    # 牛丼
-    for product in soup.find_all("div", class_="product_menu"):
-        for i in product.find_all("li", class_="menu_item"):
-            try:
-                # print(i.find("a")['href'])
-                urls.append(base + i.find("a")['href'])
-            except TypeError as e:
-                print(e)
 
-    menu_urls = ['https://www.sukiya.jp/menu/in/tondon/',
-                 'https://www.sukiya.jp/menu/in/sukimix/',
-                 'https://www.sukiya.jp/menu/in/unagi/',
-                 'https://www.sukiya.jp/menu/in/karubi/',
-                 'https://www.sukiya.jp/menu/in/curry/',
-                 'https://www.sukiya.jp/menu/in/don/',
-                 'https://www.sukiya.jp/menu/in/special/',
-                 'https://www.sukiya.jp/menu/in/kids/',
-                 'https://www.sukiya.jp/menu/in/side/',
-                 'https://www.sukiya.jp/menu/in/drink/']
+    if soup.find_all("div", class_="product_menu") is not None:
 
-    for url in menu_urls:
-        res = requests.get(url)
-        soup = BeautifulSoup(res.text, 'html.parser')
+        for product in soup.find_all("div", class_="product_menu"):
+            for i in product.find_all("li", class_="menu_item"):
+                try:
+                    # print(i.find("a")['href'])
+                    urls.append(base + i.find("a")['href'])
+                except TypeError as e:
+                    print(e)
+    else:
         product = soup.find("div", class_="sec_product_table")
 
-        # それ以外
         for i in product.find_all("td", class_="cell_product"):
             try:
                 print(i.find("a")['href'])
                 urls.append(base + i.find("a")['href'])
             except TypeError as e:
                 print(e)
+
+
+with open('menu.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['#Last Update:'+str(dt_now)])
+    writer.writerow(['#menu_name', 'category', 'price', 'kcal'])
 
     for u in urls:
         res = requests.get(u)
